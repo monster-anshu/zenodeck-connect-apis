858f6decdb2f9a7bac1a6a56c1e704303faeece2
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-26 09:12:34 +0000

    add swagger and zod

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,32 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { patchNestJsSwagger, ZodValidationPipe } from 'nestjs-zod';
 import { AppModule } from '~/app.module';
 import { PORT } from '~/env';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter({ logger: true })
   );
+
+  app.setGlobalPrefix('/api/v1/connect');
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Zenodeck Connect Apis')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+    jsonDocumentUrl: 'swagger.json',
+  });
+
   await app.listen(PORT, '0.0.0.0');
 }
 
+patchNestJsSwagger();
 bootstrap();
