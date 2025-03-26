b08e368d2f4394a648129bbf78f75bf6884d6b0f
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-26 09:12:34 +0000

    add dotenv

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+export const PORT = +(process.env.PORT || 3000);
+export const STAGE = process.env.STAGE as string;
+export const NODE_ENV = process.env.NODE_ENV;
+
+export const CONNECT_APP_API_KEY = process.env.CONNECT_APP_API_KEY as string;
+export const CONNECT_API_URL = process.env.CONNECT_API_URL as string;
+
+export const MONGO_DEFAULT_URI = process.env.MONGO_DEFAULT_URI as string;
+export const MONGO_COMMON_URI = process.env.MONGO_COMMON_URI as string;
+
+export const ENCRYPTION_IV = process.env.ENCRYPTION_IV as string;
+export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY as string;
+
+export const SESSION_JWT_SECRET = process.env.SESSION_JWT_SECRET as string;
+export const ROOT_DOMAIN = process.env.ROOT_DOMAIN as string;
+export const USER_SERVICE_DOMAIN = 'accounts.' + ROOT_DOMAIN;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '~/app.module';
+import { PORT } from '~/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
 bootstrap();
