50143e663c6f0b76bed22e9013a83e35b86c9241
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-03-26 09:12:34 +0000

    add internal module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { InternalModule } from './internal/internal.module';
 
 @Module({
-  imports: [],
+  imports: [InternalModule],
   controllers: [AppController],
   providers: [],
 })
