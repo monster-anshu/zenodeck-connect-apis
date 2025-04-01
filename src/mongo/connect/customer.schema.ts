71ece95ee52e7d60f066674d591fe3d153e4d545
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-04-01 09:38:04 +0000

    feat: add customer schema

---
+++ src/mongo/connect/customer.schema.ts
@@ -0,0 +1,62 @@
+import { Schema } from 'mongoose';
+import { MONGO_CONNECTION } from '../connections';
+
+const CUSTOMER_STATUS = ['ACTIVE', 'DELETED'] as const;
+const CUSTOMER_ONLINE_STATUS = ['ONLINE', 'AWAY', 'OFFLINE'] as const;
+
+const FieldSchema = new Schema({} as Record<string, string>, {
+  strict: false,
+  _id: false,
+});
+
+const CustomerSchema = new Schema(
+  {
+    appId: {
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
+    channelId: {
+      required: true,
+      type: Schema.Types.ObjectId,
+    },
+    deletedBy: {
+      type: Schema.Types.ObjectId,
+    },
+    fields: FieldSchema,
+    lastSeenAt: {
+      type: Date,
+    },
+    onlineStatus: {
+      default: 'AWAY',
+      enum: CUSTOMER_ONLINE_STATUS,
+      required: true,
+      type: String,
+    },
+    source: {
+      required: true,
+      type: String,
+    },
+    status: {
+      default: 'ACTIVE',
+      enum: CUSTOMER_STATUS,
+      required: true,
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    autoIndex: false,
+  }
+);
+
+const CustomerSchemaName = 'customer';
+
+const CustomerModel = MONGO_CONNECTION.DEFAULT.model(
+  'customer',
+  CustomerSchema
+);
+
+export const CustomerSchemaProvider = {
+  provide: CustomerSchemaName,
+  useValue: CustomerModel,
+};
