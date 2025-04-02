92db21ed340e193d4df819a20c9fa3496fa5e268
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-04-02 08:34:58 +0000

    add health post route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+function getRandomBetween(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
 
 @Controller()
 export class AppController {
   @Get()
   health(): string {
+    return 'ok';
+  }
+
+  @Post()
+  async healthPost() {
+    const sec = getRandomBetween(0, 10);
+    await delay(sec * 1000);
     return 'ok';
   }
 }
