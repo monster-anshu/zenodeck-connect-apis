64e09987fb66ec305fff9d064080a32be627f414
Himanshu Gunwant <hgunwant2312@gmail.com>
2025-04-05 07:01:58 +0000

    feat: add chat controller for agent

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AgentGuard } from '~/agent/agent.guard';
+import { GetSession } from '~/session/session.decorator';
+import { ChatService } from './chat.service';
 
+@UseGuards(AgentGuard)
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Get()
+  async list(@GetSession('appId') appId: string) {
+    const chats = await this.chatService.list(appId, {});
+
+    return {
+      isSuccess: true,
+      chats,
+    };
+  }
+}
